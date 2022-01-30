cb2cbd055dcb622b99c65633b2fe3fe1552a27b3
Stephen Brown-Bourne <sbrownbourne@gmail.com>
2022-01-30 12:01:59 -0800

    interactive graph, no more high points

--- transform.py
+++ transform.py
@@ -44,11 +44,6 @@ parser.add_argument(
     "file_path", metavar="file", help="a path to the file to run the fourier transform on"
 )
 parser.add_argument(
-    "threshold",
-    type=float,
-    help="the threshold",
-)
-parser.add_argument(
     "-n",
     "--no-graph",
     action="store_true"
@@ -68,34 +63,60 @@ file.close()
 
 # perform calculations
 transform = fft2(matrix)
-transform_without_peaks = deepcopy(transform)
-max_value = args.threshold
-for row in range(len(transform_without_peaks)):
-    for col in range(len(transform_without_peaks[row])):
-        if abs(np.real(transform_without_peaks[row][col])) > max_value:
-            transform_without_peaks[row][col] = 0
-reverse = ifft2(transform_without_peaks)
+# transform_without_peaks = deepcopy(transform)
+# max_value = args.threshold
+# for row in range(len(transform_without_peaks)):
+#     for col in range(len(transform_without_peaks[row])):
+#         if abs(np.real(transform_without_peaks[row][col])) > max_value:
+#             transform_without_peaks[row][col] = 0
+# reverse = ifft2(transform_without_peaks)
 
+transformed_data = deepcopy(transform)
+transformed_back_data = ifft2(transformed_data)
+
+points_removed = {}
 
 # only plot if the no_graph option hasn't been specified
 if not no_graph:
     # plot everything
-    figure, (axis1, axis2, axis3, axis4,) = plt.subplots(4, 1)
+    figure, (axis1, axis2, axis3,) = plt.subplots(3, 1)
     axis1.set_title('Original Data')
     axis1.imshow(matrix)
     axis2.set_title('Fourier Transform')
-    axis2.imshow(np.real(transform))
-    axis3.set_title('Remove "Dots"')
-    axis3.imshow(np.real(transform_without_peaks))
-    axis4.set_title('Invert Transform')
-    axis4.imshow(np.real(reverse))
+    axis2.imshow(np.real(transformed_data))
+    # axis3.set_title('Remove "Dwow"')
+    # axis3.imshow(np.real(transform_without_peaks))
+    axis3.set_title('Invert Transform')
+    axis3.imshow(np.real(transformed_back_data))
+
+    LEFT_CLICK = 1
+    def on_click(event):
+        if event.button == LEFT_CLICK and event.inaxes == axis2:
+            x = round(event.xdata)
+            y = round(event.ydata)
+            formatted_point = f"[{x},{y}]"
+
+            if points_removed.get(formatted_point):
+                value_to_restore = points_removed.pop(formatted_point)
+                transformed_data[y][x] = value_to_restore;
+            else:
+                points_removed[formatted_point] = transformed_data[y][x]
+                transformed_data[y][x] = 0;
+
+            # recaculate transforms and re-draw graphs:
+            axis2.imshow(np.real(transformed_data))
+            transformed_back_data = ifft2(transformed_data)
+            axis3.imshow(np.real(transformed_back_data))
+            plt.gcf().canvas.draw_idle()
+
+    connection_id = figure.canvas.mpl_connect('button_press_event', on_click)
     plt.show()
+    figure.canvas.mpl_disconnect(connection_id)
 
 
-formatted_max_value = "{:.2e}".format(max_value) # use sci notation
 # write data to output file
-with open(f"{'.'.join(file_path.split('.')[:-1])}_no_peaks_{formatted_max_value}.csv", 'w', encoding='UTF8') as f:
+with open(f"{'.'.join(file_path.split('.')[:-1])}_sans{'_'.join(points_removed.keys())}.csv", 'w', encoding='UTF8') as f:
     writer = csv.writer(f)
     # write the data
-    for row in np.real(reverse):
+    for row in np.real(transformed_back_data):
         writer.writerow(row)
