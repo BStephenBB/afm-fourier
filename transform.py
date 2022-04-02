a719285837ad2abb2458e2fe2075fee2bb05eec0
Stephen Brown-Bourne <sbrownbourne@gmail.com>
2022-04-02 14:26:01 -0700

    add export button

--- transform.py
+++ transform.py
@@ -2,13 +2,18 @@ from scipy.fft import fft2
 from copy import deepcopy
 from typing import Any
 from re import sub, MULTILINE
-import sys
 import os
 from scipy.fft import ifft2
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Button
 import numpy as np
 import argparse
-import csv  
+import csv
+
+# TODOs
+# show the imaginary number graph (and allow for those numbers to be blanked out too)
+# make the graphs bigger
+# higher contrast in graphs, maybe use a log scale for the FFT graph
 
 
 def parse_text_blob(blob):
@@ -39,23 +44,23 @@ def parse_text_blob(blob):
 
 
 # accept and parse cli input
-parser = argparse.ArgumentParser(description="Perform a fourier transform and filter on a file")
-parser.add_argument(
-    "file_path", metavar="file", help="a path to the file to run the fourier transform on"
+parser = argparse.ArgumentParser(
+    description="Perform a fourier transform and filter on a file"
 )
 parser.add_argument(
-    "-n",
-    "--no-graph",
-    action="store_true"
+    "file_path",
+    metavar="file",
+    help="a path to the file to run the fourier transform on",
 )
+parser.add_argument("-n", "--no-graph", action="store_true")
 args = parser.parse_args()
 file_path = args.file_path
 no_graph = args.no_graph
 
- 
-# parse file 
-assert os.path.exists(file_path), "File count not be found at: "+str(file_path)
-file = open(file_path,'r+')
+
+# parse file
+assert os.path.exists(file_path), "File count not be found at: " + str(file_path)
+file = open(file_path, "r+")
 text = file.read()
 matrix = parse_text_blob(text)
 file.close()
@@ -77,6 +82,7 @@ final_data = None
 
 points_removed = {}
 
+
 class Writer:
     def __init__(self, data):
         self.data = data
@@ -86,11 +92,19 @@ class Writer:
 
     def write(self):
         # write data to output file
-        with open(f"{'.'.join(file_path.split('.')[:-1])}_sans{'_'.join(points_removed.keys())}.csv", 'w', encoding='UTF8') as f:
+        with open(
+            f"{'.'.join(file_path.split('.')[:-1])}_sans{'_'.join(points_removed.keys())}.csv",
+            "w",
+            encoding="UTF8",
+        ) as f:
             writer = csv.writer(f)
             # write the data
             for row in np.real(self.data):
                 writer.writerow(row)
+
+    def write_handler(self, _event):
+        # need to define this method since the on_clicked callback accepts self and event as args (even though we don't use the event)
+        self.write()
 
 
 writer = Writer(transformed_back_data)
@@ -99,16 +113,17 @@ writer = Writer(transformed_back_data)
 if not no_graph:
     # plot everything
     figure, (axis1, axis2, axis3,) = plt.subplots(3, 1)
-    axis1.set_title('Original Data')
+    axis1.set_title("Original Data")
     axis1.imshow(matrix)
-    axis2.set_title('Fourier Transform')
+    axis2.set_title("Fourier Transform")
     axis2.imshow(np.real(transformed_data))
     # axis3.set_title('Remove "Dwow"')
     # axis3.imshow(np.real(transform_without_peaks))
-    axis3.set_title('Invert Transform')
+    axis3.set_title("Invert Transform")
     axis3.imshow(np.real(transformed_back_data))
 
     LEFT_CLICK = 1
+
     def on_click(event):
         # only toggle point for left clicks, on the correct axis, and for double clicks
         if event.button == LEFT_CLICK and event.inaxes == axis2 and event.dblclick:
@@ -118,10 +133,10 @@ if not no_graph:
 
             if points_removed.get(formatted_point):
                 value_to_restore = points_removed.pop(formatted_point)
-                transformed_data[y][x] = value_to_restore;
+                transformed_data[y][x] = value_to_restore
             else:
                 points_removed[formatted_point] = transformed_data[y][x]
-                transformed_data[y][x] = 0;
+                transformed_data[y][x] = 0
 
             # recaculate transforms and re-draw graphs:
             axis2.imshow(np.real(transformed_data))
@@ -130,23 +145,16 @@ if not no_graph:
             axis3.imshow(np.real(transformed_back_data))
             plt.gcf().canvas.draw_idle()
 
-    connection_id = figure.canvas.mpl_connect('button_press_event', on_click)
+    connection_id = figure.canvas.mpl_connect("button_press_event", on_click)
 
-    plt.get_current_fig_manager().full_screen_toggle() # toggle fullscreen mode
+    plt.get_current_fig_manager().full_screen_toggle()  # toggle fullscreen mode
+
+    button_axis = plt.axes([0.89, 0.05, 0.1, 0.07])
+    export_button = Button(button_axis, "Export")
+    export_button.on_clicked(writer.write_handler)
 
     plt.show()
     figure.canvas.mpl_disconnect(connection_id)
 
 
 writer.write()
-# # write data to output file
-# with open(f"{'.'.join(file_path.split('.')[:-1])}_sans{'_'.join(points_removed.keys())}.csv", 'w', encoding='UTF8') as f:
-#     writer = csv.writer(f)
-#     # write the data
-#     if final_data: 
-#         for row in np.real(final_data):
-#             writer.writerow(row)
-#     else:
-#         for row in np.real(transformed_back_data):
-#             writer.writerow(row)
-
