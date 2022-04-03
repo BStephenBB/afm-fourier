25e899564be890cba81be8f94b59d5a057760ca1
Stephen Brown-Bourne <sbrownbourne@gmail.com>
2022-04-02 23:30:28 -0700

    keep track of min and max

--- transform.py
+++ transform.py
@@ -6,14 +6,10 @@ import os
 from scipy.fft import ifft2
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Button
+from matplotlib.colors import LogNorm
 import numpy as np
 import argparse
 import csv
-
-# TODOs
-# show the imaginary number graph (and allow for those numbers to be blanked out too)
-# make the graphs bigger
-# higher contrast in graphs, maybe use a log scale for the FFT graph
 
 
 def parse_text_blob(blob):
@@ -68,18 +64,30 @@ file.close()
 
 # perform calculations
 transform = fft2(matrix)
-# transform_without_peaks = deepcopy(transform)
-# max_value = args.threshold
-# for row in range(len(transform_without_peaks)):
-#     for col in range(len(transform_without_peaks[row])):
-#         if abs(np.real(transform_without_peaks[row][col])) > max_value:
-#             transform_without_peaks[row][col] = 0
-# reverse = ifft2(transform_without_peaks)
-
 transformed_data = deepcopy(transform)
 transformed_back_data = ifft2(transformed_data)
 final_data = None
 
+# determine min and max values
+min_max = {"real": {"min": 0, "max": 0,}, "imaginary": {"min": 0, "max": 0,}}
+
+for row in np.real(transformed_data):
+    for value in row:
+        if value > min_max["real"]["max"]:
+            min_max["real"]["max"] = value
+        if value < min_max["real"]["min"]:
+            min_max["real"]["min"] = value
+
+# determine min and max values
+for row in np.imag(transformed_data):
+    for value in row:
+        if value > min_max["imaginary"]["max"]:
+            min_max["imaginary"]["max"] = value
+        if value < min_max["imaginary"]["min"]:
+            min_max["imaginary"]["min"] = value
+
+
+# store "removed" points in an object here for restoring and building output file path
 points_removed = {}
 
 
@@ -109,15 +117,19 @@ class Writer:
 
 writer = Writer(transformed_back_data)
 
+
 # only plot if the no_graph option hasn't been specified
 if not no_graph:
+    log_norm = LogNorm()
     # plot everything
     figure, (axis1, axis2, axis3, axis4) = plt.subplots(4, 1)
     axis1.set_title("Original Data")
     axis1.imshow(matrix)
     axis2.set_title("Fourier Transform - Real")
+    # axis2.imshow(np.real(transformed_data), norm=log_norm, cmap="bwr")
     axis2.imshow(np.real(transformed_data))
     axis3.set_title("Fourier Transform - Imaginary")
+    # axis3.imshow(np.imag(transformed_data), norm=log_norm, cmap="bwr")
     axis3.imshow(np.imag(transformed_data))
     axis4.set_title("Invert Transform")
     axis4.imshow(np.real(transformed_back_data))
@@ -126,11 +138,16 @@ if not no_graph:
 
     def on_click(event):
         # only toggle point for left clicks, on the correct axis, and for double clicks
-        if event.button == LEFT_CLICK and event.inaxes == axis2 and event.dblclick:
+        is_left_double_click = event.button == LEFT_CLICK and event.dblclick
+        if event.button == is_left_double_click and (
+            event.inaxes == axis2 or event.inaxes == axis3
+        ):
             x = round(event.xdata)
             y = round(event.ydata)
             formatted_point = f"[{x},{y}]"
 
+            # For now, clicking on either the imaginary or real axis will remove the point completely.
+            # The same is true for restoring the points. In the future we might want to be more sophisticated and eliminate _only_ the real or imaginary part based on the graph clicked on.
             if points_removed.get(formatted_point):
                 value_to_restore = points_removed.pop(formatted_point)
                 transformed_data[y][x] = value_to_restore
@@ -140,6 +157,7 @@ if not no_graph:
 
             # recaculate transforms and re-draw graphs:
             axis2.imshow(np.real(transformed_data))
+            axis3.imshow(np.imag(transformed_data))
             transformed_back_data = ifft2(transformed_data)
             writer.update_data(transformed_back_data)
             axis4.imshow(np.real(transformed_back_data))
